5a2c3b0df44d0122ce0e1d53e253dfdec88d7bc1
Vaasuawesome7 <55234743+Vaasuawesome7@users.noreply.github.com>
2020-08-30 16:40:23 +0530

    1.0.0

--- app/src/main/java/com/example/nasagallery/NASA_JSON.java
+++ app/src/main/java/com/example/nasagallery/NASA_JSON.java
@@ -2,9 +2,9 @@ package com.example.nasagallery;
 
 import retrofit2.Call;
 import retrofit2.http.GET;
-import retrofit2.http.Path;
+import retrofit2.http.Query;
 
 public interface NASA_JSON {
     @GET("apod?api_key=kuZRpbmxczf8poKOJk7HrBAgmCDAqZvgrDVxjGo5&date={date}")
-    Call<NASAGallery> getObject(@Path("date") String date);
+    Call<NASAGallery> getObject(@Query("date") String date);
 }
