7ae7323ee4ca8320119002ac3558dc554941858b
Vaasuawesome7 <55234743+Vaasuawesome7@users.noreply.github.com>
2020-09-01 17:33:15 +0530

    1.0.5

---
+++ app/src/main/java/com/example/nasagallery/NASAImageAndVideo.java
@@ -0,0 +1,14 @@
+package com.example.nasagallery;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class NASAImageAndVideo extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_nasa_image_and_video);
+    }
+}
